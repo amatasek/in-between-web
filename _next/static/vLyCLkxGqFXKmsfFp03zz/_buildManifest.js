188362438207befe1d0d2f3b1aba520751a4874a
amatasek <amatasek@users.noreply.github.com>
2025-04-05 19:01:25 +0000

    Deploying to gh-pages from @ amatasek/in-between-web@8c3ec16fcd5aad9b9c41ac945b512ba90b41b956 🚀

--- _next/static/vLyCLkxGqFXKmsfFp03zz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/430-c682848dd2b7615b.js","static/css/61e9fd3c2d6f5606.css","static/chunks/pages/index-134b44c32595b0f9.js"],"/_error":["static/chunks/pages/_error-040e4977a8a65860.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
