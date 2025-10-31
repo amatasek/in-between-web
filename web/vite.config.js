06a5c78b8e990225a0255e7b39b11c7a0beddbe9
Andrew Matasek <amatasek@outlook.com>
2025-10-31 01:29:07 -0500

    File static icons

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'icons/*',
+          dest: 'icons'
+        }
+      ]
+    })
   ],
   resolve: {
     alias: {
